4b808d9ddcfb8c53e85789032f3448d8019ef6b6
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-05-13 12:13:29 -0300

    Initial commit

---
+++ src/main/kotlin/ingsis/snippetpermission/SnippetPermissionApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.snippetpermission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetPermissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetPermissionApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/snippetpermission/SnippetPermissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.snippetpermission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetPermissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
